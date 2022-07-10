b37c724992c622929d058e26e81650581ef50bc0
arafat-alim <arafat97alim@gmail.com>
2022-07-11 00:32:49 +0530

    added dark mode toggler

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
+import React, { useState } from "react";
 import Main from "./components/Main";
 import Navbar from "./components/Navbar";
 
 function App() {
+  const [mode, setMode] = useState(false);
+
+  function handleToggle() {
+    setMode((prevMode) => !prevMode);
+  }
   return (
     <div className="App">
-      <Navbar />
-      <Main />
+      <Navbar mode={mode} handleToggle={handleToggle} />
+      <Main mode={mode} />
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,22 @@
 import React from "react";
 import logo from "../images/react-icon-small.png";
 
-export default function Navbar() {
+export default function Navbar(props) {
   return (
-    <nav>
+    <nav className={props.mode ? "" : "dark"}>
       <img src={logo} alt="react-logo" className="nav--icon" />
       <h3 className="nav--title_text">React Facts</h3>
-      <h4 className="nav--title">React Course - Project 1</h4>
+      {/* <h4 className="nav--title">React Course - Project 1</h4> */}
+      {/* <button onClick={props.handleToggle}>
+        {props.mode ? "Dark Mode" : "Light Mode"}
+      </button> */}
+      <div className="toggler">
+        <p className="toggler--light">Light</p>
+        <div className="toggler--slider" onClick={props.handleToggle}>
+          <div className="toggler--slider--circle"></div>
+        </div>
+        <p className="toggler--dark">Dark</p>
+      </div>
     </nav>
   );
 }
